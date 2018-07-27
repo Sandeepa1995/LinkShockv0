4f17b78506ed249b93489368ac4020202e25e6f7
Sandeepa1995 <damithalenadora@yahoo.com.au>
2018-07-27 17:16:41 +0530

    Admin automake

--- routes/link-routes.js
+++ routes/link-routes.js
@@ -304,32 +304,32 @@ router.post('/shock',(req,res,next)=> {
 
                     new_shock.save((err) => {
                         if (err) throw err;
-                        // axios({
-                        //     method: 'post',
-                        //     url: 'https://io.adafruit.com/api/v2/Sandeepa1995/feeds',
-                        //     data: {
-                        //         name: req.body.id + '.state',
-                        //         key: req.body.ada_key + '.state'
-                        //     },
-                        //     headers: {'Content-Type': 'application/json', 'X-AIO-Key': '547b680e533849f9a9a8f096d6ae1e9c'}
-                        // }).then((response) => {
-                        // })
-                        //     .catch(function (error) {
-                        //         console.log(error);
-                        //     });
-                        // axios({
-                        //     method: 'post',
-                        //     url: 'https://io.adafruit.com/api/v2/Sandeepa1995/feeds',
-                        //     data: {
-                        //         name: req.body.id + '.values',
-                        //         key: req.body.ada_key + '.values'
-                        //     },
-                        //     headers: {'Content-Type': 'application/json', 'X-AIO-Key': '547b680e533849f9a9a8f096d6ae1e9c'}
-                        // }).then((response) => {
-                        // })
-                        //     .catch(function (error) {
-                        //         console.log(error);
-                        //     });
+                        axios({
+                            method: 'post',
+                            url: 'https://io.adafruit.com/api/v2/Sandeepa1995/feeds',
+                            data: {
+                                name: req.body.id + '.state',
+                                key: req.body.ada_key + '.state'
+                            },
+                            headers: {'Content-Type': 'application/json', 'X-AIO-Key': '547b680e533849f9a9a8f096d6ae1e9c'}
+                        }).then((response) => {
+                        })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+                        axios({
+                            method: 'post',
+                            url: 'https://io.adafruit.com/api/v2/Sandeepa1995/feeds',
+                            data: {
+                                name: req.body.id + '.values',
+                                key: req.body.ada_key + '.values'
+                            },
+                            headers: {'Content-Type': 'application/json', 'X-AIO-Key': '547b680e533849f9a9a8f096d6ae1e9c'}
+                        }).then((response) => {
+                        })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
                         return res.json({success:true,msg:"Shock successfully added to the system."});
                     });
                 });
